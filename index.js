41f7c5c2f9b17bb1b0840b731fbf6e95612fc14e
Robert <bates.to.infinity@gmail.com>
2021-10-12 22:20:13 -0500

    commit tree 34fc33cfd58113d2f231a3bc712025c0e0f84eb6

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 function distanceFromHqInBlocks(someValue) {
-    return someValue
+    if (someValue > 42){
+        return someValue - 42;
+    }
+    else {
+        return 42 - someValue;
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    if (someValue > 42){
+        return (someValue - 42)*264;
+    }
+    else {
+        return (42 - someValue)*264;
+    }
+}
+
+function distanceTravelledInFeet(x, y) {
+    if (y < x){
+        return (x - y)*264;
+    }
+    else {
+        return (y-x)*264;
+    }
+}
+
+function calculatesFarePrice(x, y) {
+
+    const fare = distanceTravelledInFeet(x, y)
+    if (fare <= 400) {
+        return 0;
+    }
+    else if (fare > 400 && fare <= 2000){
+        return (fare - 400)*.02
+    }
+    else if (fare > 2000 && fare <=2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far'
+    }
 }
